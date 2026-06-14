da364edb1d2ed9c405ff07279d6504aed72f2731
Jack Tang <jack.zzz.tang.zzz@gmail.com>
2026-06-14 19:29:31 -0300

    feat: add Lenis

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,9 +1,10 @@
 import Home from "./Home";
+import { ReactLenis } from "lenis/react";
 
 export default function App() {
   return (
-    <>
+    <ReactLenis root>
       <Home></Home>
-    </>
+    </ReactLenis>
   );
 }
